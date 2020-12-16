44a8db3ef2d2acf6372319297926fc6baf43584b
tothadrian <tothadrian.g@gmail.com>
2020-12-16 18:29:54 +0100

    plugin for defining initial velocity of model

---
+++ rrbot_gazebo/src/model_push.cpp
@@ -0,0 +1,42 @@
+#include <functional>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+
+namespace gazebo
+{
+  class ModelPush : public ModelPlugin
+  {
+    public: ModelPush() : ModelPlugin()
+    {
+        printf("Model Push plugin started\n");
+    }
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+    {
+      // Store the pointer to the model
+      this->model = _parent;
+
+      // Listen to the update event. This event is broadcast every
+      // simulation iteration.
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          std::bind(&ModelPush::OnUpdate, this));
+    }
+
+    // Called by the world update start event
+    public: void OnUpdate()
+    {
+      // Apply a small linear velocity to the model.
+      this->model->SetLinearVel(ignition::math::Vector3d(0, .3, 0));
+    }
+
+    // Pointer to the model
+    private: physics::ModelPtr model;
+
+    // Pointer to the update event connection
+    private: event::ConnectionPtr updateConnection;
+  };
+
+  // Register this plugin with the simulator
+  GZ_REGISTER_MODEL_PLUGIN(ModelPush)
+}

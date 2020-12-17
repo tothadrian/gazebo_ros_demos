b4b1bd4e356409d571226c62407322fa1267fc44
tothadrian <tothadrian.g@gmail.com>
2020-12-17 18:52:00 +0100

    script to create and publish moving frame for conveyor belt

---
+++ rrbot_gazebo/src/conveyor_tf2_broadcast.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "my_tf2_broadcaster");
+  ros::NodeHandle node;
+
+   tf2_ros::TransformBroadcaster tfb;
+  geometry_msgs::TransformStamped transformStamped;
+
+  ros::Time start_time = ros::Time::now();
+  transformStamped.header.frame_id = "link1";
+  transformStamped.child_frame_id = "conveyor";
+  transformStamped.transform.translation.x = 0.0;
+  transformStamped.transform.translation.y = 0.0;
+  transformStamped.transform.translation.z = 0.0;
+  tf2::Quaternion q;
+        q.setRPY(0, 0, 0);
+  transformStamped.transform.rotation.x = q.x();
+  transformStamped.transform.rotation.y = q.y();
+  transformStamped.transform.rotation.z = q.z();
+  transformStamped.transform.rotation.w = q.w();
+
+  ros::Rate rate(10.0);
+  while (node.ok()){
+    ros::Duration delta_t = ros::Time::now() - start_time;
+    double delta_t_sec = delta_t.toSec();
+    transformStamped.transform.translation.y = 0.1*delta_t_sec;
+    transformStamped.header.stamp = ros::Time::now();
+    tfb.sendTransform(transformStamped);
+    rate.sleep();
+    printf("sending\n");
+  }
+
+};

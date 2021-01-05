adc8571c766e687252cd300fad69e2b82ca988c6
tothadrian <tothadrian.g@gmail.com>
2021-01-05 19:51:56 +0100

    listen to conveyor to base_link tf and calculate the passthrough filter accordingly

--- rrbot_gazebo/src/pcl_filter.cpp
+++ rrbot_gazebo/src/pcl_filter.cpp
@@ -1,6 +1,10 @@
 #include <pcl/point_cloud.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
 #include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Twist.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <iostream>
@@ -9,15 +13,44 @@ ros::Publisher pub;
 
 void process_cloud (const pcl::PCLPointCloud2ConstPtr& cloud)
 {
-  // Create container variables
-  pcl::PCLPointCloud2 filtered_pc;
+    // create node for listening to transform between base_link and conveyor_reference
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+
+  // ros::Rate rate(10.0);
+  bool success=false;
+  geometry_msgs::TransformStamped transformStamped;
+  while (!success){
+    
+    try{
+      transformStamped = tfBuffer.lookupTransform("base_link", "conveyor_reference", ros::Time(0));
+      success=true;
+    }
+    catch (tf2::TransformException &ex) {
+      ROS_WARN("%s",ex.what());
+      success=false;
+      ros::Duration(1.0).sleep();
+      continue;
+    }
+  }
+
 
   // Create filter
   pcl::PassThrough<pcl::PCLPointCloud2> pass;
   pass.setInputCloud (cloud);
   pass.setFilterFieldName ("y");
-  pass.setFilterLimits (-1.0, 1.0);
-  pass.filter (filtered_pc);
+  pass.setFilterLimits (0.0-transformStamped.transform.translation.y, 2.0-transformStamped.transform.translation.y);
+  pcl::PCLPointCloud2* pass_filter_output=new pcl::PCLPointCloud2;
+  pass.filter (*pass_filter_output);
+
+  pcl::VoxelGrid<pcl::PCLPointCloud2> voxelGrid;
+  pcl::PCLPointCloud2ConstPtr cloudPtr(pass_filter_output);
+  voxelGrid.setInputCloud(cloudPtr);
+  // set the leaf size (x, y, z)
+  voxelGrid.setLeafSize(0.01, 0.01, 0.01);
+  // apply the filter to dereferenced cloudVoxel
+  pcl::PCLPointCloud2 filtered_pc;
+  voxelGrid.filter(filtered_pc);
 
   // Publish the data
   pub.publish (filtered_pc);
